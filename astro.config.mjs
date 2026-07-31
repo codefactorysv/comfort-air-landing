302e291515dbbf390707e37a7e435ff7e72fe78d
codefactorysv <codefactorysv@users.noreply.github.com>
2026-07-30 22:28:33 -0600

    Publish HVAC landing page

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+export default defineConfig({
+  site: 'https://codefactorysv.github.io',
+  base: '/comfort-air-landing',
+  output: 'static'
+});
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const businessConfig = {
+  name: 'Comfort Air Solutions',
+  phoneDisplay: '(866) 555-0148',
+  phoneHref: '+18665550148',
+  tagline: 'Heating & Air Conditioning',
+  homesServed: '10,000+'
+};
